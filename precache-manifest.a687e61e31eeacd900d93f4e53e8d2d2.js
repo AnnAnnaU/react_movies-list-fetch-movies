e00a0422c89851724890c61c6846ce2dd6de89fe
Home <Home@LT-259286.kyiv.ciklum.net>
2021-02-24 17:23:22 +0200

    Updates

---
+++ precache-manifest.a687e61e31eeacd900d93f4e53e8d2d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93f3cb1bd61daa219ff00f07e2d846d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4411ee41196d106f38f9",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "630e021703f517322f63",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "4411ee41196d106f38f9",
+    "url": "./static/js/2.b9aba1d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b9aba1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630e021703f517322f63",
+    "url": "./static/js/main.1c98ca10.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
